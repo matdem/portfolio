4ef015cfcfe502623c2de96efe5a2628eada7405
matdem <math.demers@protonmail.com>
2020-08-31 21:58:53 -0400

    Fix a bug with the width's nav when window width changed.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let navElt = document.getElementById("nav-content"),
   closeBtnElt = document.getElementById("closeBtn"),
   iconMenuElt = document.getElementById("icon-menu"),
-  anchorNavElts = document.getElementsByClassName("a-nav");
+  anchorNavElts = document.getElementsByClassName("a-nav"),
+  smallerScreenWidth = window.matchMedia("(max-width: 600px)");
 
 function openNav() {
   navElt.style.width = "50vw";
@@ -19,6 +20,16 @@ iconMenuElt.addEventListener("click", openNav);
 
 closeBtnElt.addEventListener("click", closeNav);
 
-for (let i = 0; i < anchorNavElts.length; i++) {
-  anchorNavElts[i].addEventListener("click", closeNav);
-}
+smallerScreenWidth.addEventListener("change", function (mm) {
+  if (mm.matches) {
+    for (let i = 0; i < anchorNavElts.length; i++) {
+      anchorNavElts[i].addEventListener("click", closeNav);
+    }
+    navElt.style.width = "0";
+  } else {
+    for (let i = 0; i < anchorNavElts.length; i++) {
+      anchorNavElts[i].removeEventListener("click", closeNav);
+    }
+    navElt.style.width = "18rem";
+  }
+});
